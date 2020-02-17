87b7c35c4967c00183ed4282c0475ce239ef2838
Ilia FILIN <ilia@filin.eu>
2020-02-17 18:51:59 +0100

    Adding high level structure

---
+++ app.test.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect
+
+describe("Rover", () => {
+	it.skip("should be able to init", () => {})
+	it.skip("should be able to move", () => {})
+	it.skip("should be able to turn right", () => {})
+	it.skip("should be able to turn left", () => {})
+})
+
+describe("Plateau", () => {
+	it.skip("should be able to init", () => {})
+})
+
+describe("Application", () => {
+	it.skip("should be able to parse single input", () => {})
+	it.skip("should be able to parse multiple inputs", () => {})
+	it.skip("should be able to parse input with error - 1", () => {})
+	it.skip("should be able to parse input with error - 2", () => {})
+	it.skip("should be able to parse input with error - n", () => {})
+	it.skip("should be able to detect, if the rover is off the plateau", () => {})
+})
